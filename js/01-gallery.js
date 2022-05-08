372afeed638beadbc6222ec91a818771e2314c8b
Borys <92867567+BorysSabov@users.noreply.github.com>
2022-05-08 23:00:44 +0300

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const newPictureEl = galleryItems.map(option => {
+    const newEl = document.createElement('div')
+    newEl.classList.add('gallery__item');
+    console.log(newEl);
+    newEl.insertAdjacentHTML('beforebegin','<a></a>');
+});
+
+
+// https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js
