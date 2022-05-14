2070c152fab46ff5f9ae1f42a7842e91b8823534
Borys <92867567+BorysSabov@users.noreply.github.com>
2022-05-14 17:11:19 +0300

    hw-in-process

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
+const addGalleryItems = document.querySelector('.gallery');
 
-console.log(galleryItems);
-const newPictureEl = galleryItems.map(option => {
-    const newEl = document.createElement('div')
-    newEl.classList.add('gallery__item');
-    console.log(newEl);
-    newEl.insertAdjacentHTML('beforebegin','<a></a>');
+const createGalleryEl = galleryItems.map( item => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    />
+    </a>
+    </div>`
+    )
+    .join('');
+    
+    addGalleryItems.innerHTML = createGalleryEl;
+    // addGalleryItems.addEventListener('click', holdGalleryClick);
+
+    function onOpenModalImg(evt) {
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  let valueImg = evt.target.closest('.gallery__image');
+        valueImg = basicLightbox.create(`
+    <img
+      src="${valueImg.dataset.source}"
+    />
+    `);
+        valueImg.show();
+}
+
+addGalleryItems.addEventListener('click', (event) => {
+    event.preventDefault();
+    onOpenModalImg();
+    console.log(event);
 });
 
 
-// https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js
+// function onCloseModalImg(event) {
+
+// }
+
+
+// function holdGalleryClick(evt) {
+//     evt.preventDefault();
+//     const isGalleryImgBigEl = evt.target.clasList.contains('.gallery__image');
+//     if (!isGalleryImgBigEl) {
+//         return;
+//     }
+//     const isGalleryImgValue = evt.target;
+//     let imgValue = isGalleryImgValue.closest('.gallery__image');
+//     imgValue = basicLightbox.create(`<img width = "1280" height = "800" src = ${imgValue.dataset.source}>`).show()
+// };
+
+
+
+
+
