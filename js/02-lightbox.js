fac6a5fea2276bf0ba61d4b052e1636572cacc15
Borys <3040sabov@gmail.com>
2023-04-01 12:17:51 +0300

    2-task-not-add-description-yet

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const addGalleryPic = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createGalleryEl = galleryItems
+  .map(
+    (item) =>
+      ` <li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      />
+   </a>
+</li>`
+  )
+  .join("");
+
+addGalleryPic.innerHTML = createGalleryEl;
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  // captionType: "text",
+});
+console.log(gallery);
+// gallery.on("open.simplelightbox", function () {
+//   console.log("Lightbox opened");
+// });
+// gallery.on("close.simplelightbox", function () {
+//   console.log("Lightbox closed");
+// });
+
+// gallery.on("next.simplelightbox", function () {
+//   console.log("Next image");
+// });
+
+// gallery.on("prev.simplelightbox", function () {
+//   console.log("Previous image");
+// });
+// };
